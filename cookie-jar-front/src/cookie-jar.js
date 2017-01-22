64584cd08b821fbb25adf4c98801048b74619ad1
Alan Kebert <adkebert@yahoo.com>
2017-01-21 20:37:59 -0500

    AJAX is working correctly on front end

--- cookie-jar-front/src/cookie-jar.js
+++ cookie-jar-front/src/cookie-jar.js
@@ -2,23 +2,46 @@ import React, { Component } from 'react';
 import Cookie from './cookie.js'
 
 class CookieJar extends Component {
+  constructor () {
+      super()
+      this.state = {
+        cookieList: [],
+      }
+  };
+
+  componentWillMount() {
+    // With AJAX
+    // fetch('http://localhost:3001/cookies.json', {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // }).then(res => res.json())
+    //   .then(data => this.setState({
+    //                   cookieList: data
+    //                 })
+    //   );
+
+      // Default values
+    this.setState({
+      cookieList: [
+        {key:1, value:"Oreo"},
+        {key:2, value:"Oatmeal"},
+        {key:3, value:"Sugar"},
+        {key:4, value:"Chocolate"},
+      ]
+    });
+  }
+
   render() {
     const CookieJarStyle = {
       'display' : 'flex',
       'justifyContent' : 'spaceBetween',
       'width' : '200px',
       'flexWrap' : 'wrap',
-    }
+    };
 
-    const CookieList = [
-      "Oreo",
-      "Oatmeal",
-      "Sugar",
-      "Chocolate",
-    ];
-
-    var theCookies = CookieList.map(function(cookie) {
-      return <Cookie name={cookie} />;
+    var theCookies = this.state.cookieList.map(function(cookie) {
+      return <Cookie key={cookie.key} name={cookie.value} />;
     });
 
     return(
